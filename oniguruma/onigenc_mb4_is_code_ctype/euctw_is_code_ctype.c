4f527ada1cd38de7007bd480bb1a3831688b7cff
Ambrose <96902760+ambrosecm@users.noreply.github.com>
2025-03-21 11:23:55 +0800

    Add files via upload

---
+++ oniguruma/onigenc_mb4_is_code_ctype/euctw_is_code_ctype.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "euc_tw.c"
+#include "regenc.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+
+    // 打开文件
+    FILE *inputFile = fopen(argv[1], "r");
+    if (!inputFile) {
+        fprintf(stderr, "Error opening file: %s\n", argv[1]);
+        return 1;
+    }
+
+    unsigned int code;
+    unsigned int ctype;
+
+    // 读取文件中的两个整数
+    if (fscanf(inputFile, "%u %u", &code, &ctype) != 2) {
+        fprintf(stderr, "Error reading integers from file.\n");
+        fclose(inputFile);
+        return 1;
+    }
+
+    // 关闭文件
+    fclose(inputFile);
+
+    // 调用 euctw_is_code_ctype 函数
+    int result = euctw_is_code_ctype(code, ctype);
+
+    // 输出结果
+    printf("Result: %d\n", result);
+
+    return 0;
+}
