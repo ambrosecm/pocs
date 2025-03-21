4c36509caea4d8f5aa7f7ab5cbcc242dba7ebe2c
Ambrose <96902760+ambrosecm@users.noreply.github.com>
2025-03-21 14:25:50 +0800

    Add files via upload

---
+++ oniguruma/koi8_is_code_ctype/koi8_is_code_ctype.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "koi8.c"
+#include "regenc.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+
+    // 打开文件
+    FILE *inputFile = fopen(argv[1], "r");
+    if (!inputFile) {
+        fprintf(stderr, "Error opening file: %s\n", argv[1]);
+        return 1;
+    }
+
+    int code;
+    int ctype;
+
+    // 读取文件中的两个整数
+    if (fscanf(inputFile, "%d %d", &code, &ctype) != 2) {
+        fprintf(stderr, "Error reading integers from file.\n");
+        fclose(inputFile);
+        return 1;
+    }
+
+    // 关闭文件
+    fclose(inputFile);
+
+    // 调用 koi8_is_code_ctype 函数
+    int result = koi8_is_code_ctype(code, ctype);
+
+    // 输出结果
+    printf("Result: %d\n", result);
+
+    return 0;
+}
